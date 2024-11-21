bd16629962746f0cb8230731adb029941234f867
HyunSeung <lleen0114@naver.com>
2024-11-22 03:41:08 +0900

    Feat: Project Setting

---
+++ PersonalPJT/src/main/java/org/example/personalpjt/PersonalPjtApplication.java
@@ -0,0 +1,13 @@
+package org.example.personalpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalPjtApplication.class, args);
+	}
+
+}
---
+++ PersonalPJT/src/test/java/org/example/personalpjt/PersonalPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.personalpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
